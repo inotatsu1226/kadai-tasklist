ef972286e9d3a1e05f0d170f9545fd5113781494
TatsuyaInoue <inotatsu1226@gmail.com>
2017-04-21 00:15:11 +0000

    Lesson12-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  #root to: 'tasks#index'
+   root to: 'toppages#new'
+  
+  get 'login', to: 'toppages#new'
+  post 'login', to: 'toppages#create'
+  delete 'logout', to: 'toppages#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
